047443c365eb8d0a993ab58a4d5db651802b82a9
Ahmed Atwa <ahmedatwa866@yahoo.com>
2022-09-20 16:11:28 +0200

    HTML for table page

--- frontend-nextjs/pages/table/index.tsx
+++ frontend-nextjs/pages/table/index.tsx
@@ -2,6 +2,83 @@
 export default function Table() {
 
     return (
-        <h1>Table Page</h1>
+        <>
+            <header>
+                <div>
+                    <div>
+                        <h2>Table Title</h2>
+                    </div>
+                    <div>
+                        <button>File</button>
+                        <button>File</button>
+                        <button>File</button>
+                        <button>File</button>
+                        <button>File</button>
+                    </div>
+                </div>
+                <div>
+                    <img src="" alt=""/>
+                </div>
+            </header>
+            <main>
+                <div>
+                    <div>
+                        <input />
+                    </div>
+                    <div>
+                        <section>
+                            <h3>Pending</h3>
+                            <p>Mohamed Alaa &rarr;</p>
+                        </section>
+                        <section>
+                            <h3>Accepted</h3>
+                            <p>Sami Zayn ▶</p>
+                        </section>
+                        <section>
+                            <h3>Refused</h3>
+                            <p>Thundertaker</p>
+                        </section>  
+                    </div>
+                    <div>
+                        <button>add</button>
+                    </div>       
+                </div>
+                <div>
+                    <div>
+                        <div>
+                            <h2>Thundertaker</h2>
+                            <h3>Track: Web. Status: Pending</h3>
+                        </div>
+                        <div>
+                            <img src="" alt=""/>
+                            controls
+                        </div>
+                    </div>
+                    <div>
+                        <section>
+                            <div>
+                                <p>Email: thunder@taker.com</p>
+                                <p>Phone: 011111111111</p>
+                            </div>
+                            <div>
+                                <p>Year: 5th</p>
+                                <p>Submission Date: 25/9/2022</p>
+                            </div>
+                        </section>
+                        <section>
+                            <h3>Experience</h3>
+                            <p>dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque</p>
+                            <h3>Interests</h3>
+                            <p>dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque</p>
+                            <h3>Notes</h3>
+                            <p>dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque</p>
+                        </section>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                
+            </footer>
+        </>
     )
 }
