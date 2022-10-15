10fc230b6a11ce45848b182e9c0d8601e0f70b35
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2022-10-15 19:49:54 +0200

    fix: remove console log

--- frontend-nextjs/utils/apis/RecruitmentDataAPI.ts
+++ frontend-nextjs/utils/apis/RecruitmentDataAPI.ts
@@ -1,10 +1,7 @@
-import data from '../data.json'
-import { IRecruitmentData } from '../types/RecruitmentDataTypes'
 import axios from 'axios'
 import config from '../config'
 
 async function getAllData() {
-    console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
     const response = await axios.get(`${config.backendUrl}/memberapplications`)
     return response.data
 }
