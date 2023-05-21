2178ce9bb3edb6c84c3e43dd0e5cddabd0de2040
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-05-21 16:31:50 +0300

    remove comments

--- frontend-nextjs/components/AppBar.tsx
+++ frontend-nextjs/components/AppBar.tsx
@@ -182,36 +182,6 @@ const AppBar = () => {
             </Menu>
           </Box>
 
-          {/* <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
-
         </Toolbar>
       </Container>
     </AppbarComponent>
